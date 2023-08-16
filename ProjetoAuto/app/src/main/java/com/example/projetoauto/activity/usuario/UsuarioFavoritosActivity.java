be0aaeee69d0466eef0fa48ac238b9cfa117ec36
Guilherme Brito <oguilherme.rodrigues10@gmail.com>
2023-08-16 00:56:50 -0300

    Criação tela de perfil

---
+++ ProjetoAuto/app/src/main/java/com/example/projetoauto/activity/usuario/UsuarioFavoritosActivity.java
@@ -0,0 +1,14 @@
+package com.example.projetoauto.activity.usuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsuarioFavoritosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_favoritos);
+    }
+}
